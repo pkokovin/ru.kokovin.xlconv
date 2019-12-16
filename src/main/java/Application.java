d86631304fc8da374bed605296f09935738d2f63
pashak <pkokovin@gmail.com>
2019-12-16 10:17:27 +0300

    v. 02 should not rename files before parsing, find and parse all EXEL files in current directory

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,10 +4,22 @@ import java.io.*;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        File out = new File("toparce.csv");
-        FileWriter fileWriter = new FileWriter(out);
-        fileWriter.write(Parser.parse("toparce.xls"));
-        fileWriter.flush();
-        fileWriter.close();
+        File currentdir = new File(".");
+        File[] files = currentdir.listFiles();
+        for (File inst: files) {
+            String filename = inst.getName().toLowerCase();
+            if (inst.isFile() && filename.endsWith(".xls")
+                    || filename.endsWith(".xlsx")) {
+                String outname = filename.endsWith(".xls")
+                        ? filename.replace(".xls", ".csv")
+                        : filename.replace(".xlsx", "csv");
+                File out = new File(outname);
+                try (FileWriter fileWriter = new FileWriter(out)){
+                    fileWriter.write(Parser.parse(inst.getName()));
+                    fileWriter.flush();
+                }
+            }
+        }
+
     }
 }
